1b456eb9de7edc4e3affe99b7408fd6ce069c7d1
pappik <paparao.prema75@gmail.com>
2025-03-24 20:56:17 -0500

    a framework for all tools is created prometheus tool code created gocd pipeine is created for creating tools

---
+++ module/data.tf
@@ -0,0 +1,10 @@
+data "aws_ami" "ec2" {
+  most_recent      = true
+  name_regex       = "RHEL-9-DevOps-Practice"
+  owners           = ["973714476881"]
+
+  }
+
+data "aws_security_group" "selected" {
+  name = "allow-all"
+}
---
+++ module/main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "ec2" {
+  ami           = data.aws_ami.ec2.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
+
+  tags = {
+    Name = var.tool_name
+  }
+}
+
+resource "aws_route53_record" "route" {
+  name    = var.tool_name
+  type    = "A"
+  zone_id = var.zone_id
+  records = [aws_instance.ec2.public_ip]
+  ttl     = 30
+
+}
