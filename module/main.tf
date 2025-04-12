f5789afd7de41f1e4f770403d0edb35682358749
pappik <paparao.prema75@gmail.com>
2025-04-11 20:00:14 -0500

    created a role for prometheus

--- module/main.tf
+++ module/main.tf
@@ -16,3 +16,25 @@ resource "aws_route53_record" "route" {
   ttl     = 30
 
 }
+
+resource "aws_iam_role" "role" {
+  name = "${var.tool_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "${var.tool_name}-role"
+  }
+}
